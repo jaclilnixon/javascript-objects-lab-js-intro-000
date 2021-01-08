1d89c127f6f272335d943404713cbaec199e368c
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-08 22:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = { key: value };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign ({}, key, value);
+  return Object.assign ({}, recipes, key, value);
 }
