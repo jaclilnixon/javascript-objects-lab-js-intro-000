4074a59fe57479b2e8727f07d143400aebd76df6
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-08 22:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
